c549238906ee546e87a294b7fe2f6bdb912690d7
KsenyaKoss <roxenn34@gmail.com>
2023-03-27 22:40:01 +0200

    axios added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,63 @@
-const x = 5;
+import Notiflix from 'notiflix';
+const axios = require('axios/dist/browser/axios.cjs');
+import fetchUsers from './fetchPictures';
+
+
+
+const URL = 'https://pixabay.com/api/'
+const API_KEY = '34776987-31260d6b6ec62d4f22854ea75'
+let page = 1;
+const quantity = 40;
+
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  input: document.querySelector('[name="searchQuery"]'),
+}
+  
+
+refs.form.addEventListener('submit', getPictures)
+
+function getPictures(ev){
+  ev.preventDefault()
+  refs.input = ev.target.elements[0].value
+  fetchUsers(page, quantity);
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://pixabay.com/api/?key=34776987-31260d6b6ec62d4f22854ea75&q=yellow+flowers&image_type=photo
+
+
+
+
+// const fetchUsers = async () => {
+//   try {
+//     const response = await axios.get(
+//       'https://jsonplaceholder.typicode.com/users'
+//     );
+//     const users = await response.json();
+//     return users;
+//   } catch (error) {}
+// };
